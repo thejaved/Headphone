25b6c7d6ee8923b071f85f514fd99c174c614220
Javed Khan <live.javedkhan@gmail.com>
2022-03-28 14:39:41 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("active");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+  });
+}
